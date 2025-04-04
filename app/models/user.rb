c176a96dea512c095c0278960f8ced736157dd0f
fogadOg <fogadm06@gmail.com>
2025-04-04 13:37:24 +0200

    Email check

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy  # Users upload books
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+
+  validate :email_domain_check
+
+  private
+
+  def email_domain_check
+    allowed_domain = "rubynor.com"
+    unless email.ends_with?("@#{allowed_domain}")
+      errors.add(:email, "must be a rubynor.com address")
+    end
+  end
 end
